a57d409d1329e806cad073ac9b618ae5e651a5b9
예훈 권 <lionyehun@gmail.com>
2025-07-14 11:16:05 +0900

    trascription 문법오류 수정 및 4진법 2진법 변환 파일 생성

---
+++ 4to2.py
@@ -0,0 +1,4 @@
+acid_list = {'A': 0b00, 'C': 0b01, 'G': 0b10, 'U':0b11}
+rna = input()
+for a in rna:
+    
--- transcription.py
+++ transcription.py
@@ -1,7 +1,7 @@
 #전사과정 시뮬레이션 프로그램
-acid_list = {'A':'U', 'C':'G', 'G':'C', 'T':'A'}
+base_list = {'A':'U', 'C':'G', 'G':'C', 'T':'A'}
 DNA_list = input()
 RNA_list = ''
 for a in DNA_list:
-    RNA_list += acid_list[a]
+    RNA_list += base_list[a]
 print(RNA_list)
